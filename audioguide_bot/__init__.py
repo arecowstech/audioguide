78efb5ca1b2cece781750d1ddc82e04f84452283
nikolai-in <nikolai.in@tuta.io>
2024-04-24 11:15:39 +0300

    ✨ Обновил обработку текста

--- audioguide_bot/__init__.py
+++ audioguide_bot/__init__.py
@@ -3,12 +3,16 @@ __description__ = "Телеграм бот для аудиогида."
 __version__ = "0.1.0"
 
 import click
+from html import escape as html_escape
 from os import environ
 from dotenv import load_dotenv
 import logging
-from telegram import Update
+from telegram import Update, helpers, constants
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
-from pocketbase import PocketBase
+from pocketbase import PocketBase, utils
+from bs4 import BeautifulSoup
+
+supported_tags = ["b", "strong", "i", "em", "u", "ins", "s", "strike", "del", "span", "tg-spoiler", "a", "tg-emoji", "code", "pre", "blockquote"]
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -17,11 +21,27 @@ logging.basicConfig(
 
 load_dotenv()
 
+def remove_unsupported_tags(html, supported_tags):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    for tag in soup.find_all(True):
+        if tag.name not in supported_tags:
+            tag.unwrap()
+
+    return str(soup)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE, client: PocketBase):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=client.collection("posts").get_first_list_item(f"num={str(update.message.text)}").body)
+    try:
+        message_text = client.collection("posts").get_first_list_item(f"num={str(update.message.text)}").body
+        message_text = remove_unsupported_tags(message_text, supported_tags)
+    except utils.ClientResponseError:
+        message_text = "Похоже, зааписи с таким номером нет. Попробуйте другой номер😅"
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+        text=message_text,
+        parse_mode=constants.ParseMode.HTML)
 
 @click.command()
 @click.version_option()
